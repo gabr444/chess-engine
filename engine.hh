acc4355dab6235f77b8abcab1e6791b6294f248c
gabr444 <gabrielmorck@outlook.com>
2022-12-12 20:21:34 +0100

    Update engine.hh

--- engine.hh
+++ engine.hh
@@ -7,28 +7,4 @@
 #include <cstdlib>
 #include "moves.hh"
 
-class Engine
-{
-        std::string fen; 
-        char **board;
-        Moves calculate_moves;
-        // Calculate moves. Tuple input as following: <x, y>
-        std::vector<std::tuple<int, int>> get_moves(std::tuple<int, int> pos)
-        {
-            std::vector<std::tuple<int, int>> moves;
-            auto diagonal = [] (int a) {
-                return ;
-            };
-
-            switch(tolower(board[std::get<0>(pos)][std::get<1>(pos)]))
-            {
-                case 'p':
-                    moves = calculate_moves.pawn_move(pos);
-                case 'r':
-                    moves = calculate_moves.rook_move(pos);
-            }
-
-        
-            return moves;
-        }
-};
+class Engine{};
